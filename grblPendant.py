32b9061b895677a707a3a95d08a47956778a23f8
Duane Northcutt <duane.northcutt@esperanto.ai>
2021-06-06 17:47:35 -0700

    Moved notes to README.md.

--- grblPendant.py
+++ grblPendant.py
@@ -1,29 +1,6 @@
 #!/usr/bin/env python3
 '''
 Application that connects a XHC WHB04B-4 pendant to a grbl controller
-
-Button Interpretation:
-  * Reset: ????
-  * Stop: stop everything and leave in position
-  * Feed+/-: adjust feed rate while program is running
-  * Spindle+/-: adjust spindle speed while program is running
-  * M-Home: stop and home all three axes in machine coordinate space
-  * Safe-Z: retract spindle to top of Z axis travel (home Z axis only)
-  * W-Home: stop and home all three axes in workpiece coordinate space
-  * S-on/off: toggle spindle on/off
-  * Probe-Z: run probing cycle
-  * Continuous: set into continuous movement mode
-    - selected axis will move in direction of jog wheel movement (i.e., cw or ccw)
-    - movement will stop whenever wheel movement stops
-    - movement is done at the rate given by the increment knob setting
-      * i.e., a percentage of max movement rate
-    - movement is independent of speed at which the jog wheel is rotated
-  * Step: set into step movement mode
-    - selected axis will move in direction of jog wheel movement
-    - movement is in increments given by the increment knob setting
-==> I don't know what the "Lead" setting means, will disable for now
-    I think it means that you pull the spindle around by hand and it follows
-  * Macro-[1-11]: defined by yaml in button config file
 '''
 
 import argparse
